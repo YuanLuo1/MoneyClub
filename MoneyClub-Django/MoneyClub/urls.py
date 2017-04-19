8019cc4ae8e717ab39a15ca4b9367c9bdf503316
Yuan Luo <yluo2@andrew.cmu.edu>
2017-04-19 01:40:13 -0400

    Connect dashboard to localhost.

--- MoneyClub-Django/MoneyClub/urls.py
+++ MoneyClub-Django/MoneyClub/urls.py
@@ -6,8 +6,18 @@ import MoneyClub.forms
 import django_messages.views
 
 urlpatterns = [
-     url(r'^$', MoneyClub.views.home,name='home'),
-     url(r'^login$', django.contrib.auth.views.login, {'template_name':'MoneyClub/login.html', 'authentication_form': MoneyClub.forms.CustomAuthForm}, name='login'),
-     url(r'^logout$', django.contrib.auth.views.logout_then_login, name='logout'),
+    url(r'^$', MoneyClub.views.home,name='home'),
+    url(r'^login$', django.contrib.auth.views.login, {'template_name':'MoneyClub/login.html', 'authentication_form': MoneyClub.forms.CustomAuthForm}, name='login'),
+    url(r'^logout$', django.contrib.auth.views.logout_then_login, name='logout'),
     # url(r'^register$', MoneyClub.views.register, name='register'),
+    url(r'^dashboard$',MoneyClub.views.load, name = 'dashboard'),
+    url(r'^messages/$', RedirectView.as_view(permanent=True, url='inbox/'), name='messages_redirect'),
+    url(r'^messages/inbox/$', django_messages.views.inbox, {'template_name': 'django_messages/inbox.html'}, name='messages_inbox'),
+    url(r'^messages/outbox/$', django_messages.views.outbox, {'template_name': 'django_messages/outbox.html'}, name='messages_outbox'),
+    url(r'^messages/compose/$', django_messages.views.compose, {'template_name': 'django_messages/compose.html'}, name='messages_compose'),     url(r'^messages/compose/(?P<recipient>[\w.@+-]+)/$', django_messages.views.compose, {'template_name': 'django_messages/compose.html'}, name='messages_compose_to'),
+    url(r'^messages/reply/(?P<message_id>[\d]+)/$', django_messages.views.reply, {'template_name': 'django_messages/compose.html'}, name='messages_reply'),
+    url(r'^messages/view/(?P<message_id>[\d]+)/$', django_messages.views.view, {'template_name': 'django_messages/view.html'}, name='messages_detail'),
+    url(r'^messages/delete/(?P<message_id>[\d]+)/$', django_messages.views.delete, name='messages_delete'),
+    url(r'^messages/undelete/(?P<message_id>[\d]+)/$', django_messages.views.undelete, name='messages_undelete'),
+    url(r'^messages/trash/$', django_messages.views.trash, {'template_name': 'django_messages/trash.html'}, name='messages_trash'),
 ]
--- MoneyClub-Django/MoneyClub/views.py
+++ MoneyClub-Django/MoneyClub/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 # Create your views here.
 def home(request, error_message=None):
 	return render(request, 'MoneyClub/home.html',{})
+
+def load(request):
+	return render(request, 'MoneyClub/index.html', {})
