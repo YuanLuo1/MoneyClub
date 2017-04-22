dacd5145ebc365b49bffc4b834be33aa35792a78
Yuan Luo <yluo2@andrew.cmu.edu>
2017-04-22 01:44:32 -0400

    Added route for django-messages.

--- MoneyClub-Django/webapps/urls.py
+++ MoneyClub-Django/webapps/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^moneyclub/', include('MoneyClub.urls')),
     url(r'^$', MoneyClub.views.home),
+    url(r'^messages/', include('django_messages.urls')),
 ]
 
