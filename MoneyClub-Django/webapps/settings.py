3e7486e5d47a8fd5caf84a8dcc54561ad48be133
Yuan Luo <yluo2@andrew.cmu.edu>
2017-04-21 23:06:46 -0400

    Changed the configuration for django-messages.

--- MoneyClub-Django/webapps/settings.py
+++ MoneyClub-Django/webapps/settings.py
@@ -63,7 +63,7 @@ LOGIN_REDIRECT_URL = '/moneyclub/dashboard'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR,'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -71,6 +71,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django_messages.context_processors.inbox',
             ],
         },
     },
